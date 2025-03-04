3ebce2ae7cbc0aac613ad3f184e424e42f058942
mhmd-sjad-nfsi <mhmdsjadnfysy@gmail.com>
2025-03-05 00:08:02 +0330

    1111

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,45 @@
-import React from 'react'
-import Header from './components/Header';
-import { Container } from 'react-bootstrap';
-import Footer from './components/Footer';
-import 'react-toastify/ReactToastify.css';
-import { ToastContainer } from "react-bootstrap";
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { deepPurple, amber } from "@mui/material/colors";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/ReactToastify.css";
+
+const theme = createTheme({
+  palette: {
+    primary: deepPurple,
+    secondary: amber,
+  },
+  shape: {
+    borderRadius: 12,
+  },
+  typography: {
+    fontFamily: "Roboto, Arial, sans-serif",
+  },
+});
+
 const App = () => {
   return (
-    <>
-       < Header/>
-       <main className='py-3'>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header />
+      <main className="py-3">
         <Container>
-           <Outlet />
+          <Outlet />
+          <h1>Material Design 3 در ProShop</h1>
+          <Button variant="contained" color="primary">
+          </Button>
         </Container>
-       </main>
-       <Footer/>
-       <ToastContainer/>
-    </>
+      </main>
+      <Footer />
+      <ToastContainer />
+    </ThemeProvider>
   );
 };
 
-export default App
+export default App;
